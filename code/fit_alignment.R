829985429a7768df5343f14d050387b03b69c424
georgialoukatou <georgialoukatou@gmail.com>
2021-06-09 14:15:35 +0200

    figures

--- code/fit_alignment.R
+++ code/fit_alignment.R
@@ -42,7 +42,7 @@ childes_data <- childes_data %>%
 fit_alignment <- function(alignment_df, sd = .5) {
   
 alignment_data <- list(NumMarkers = length(unique(alignment_df$category)),
-                         NumSubPops = length(unique(alignment_df$pop_)),
+                         NumSubPops = length(unique(alignment_df$subpop)),
                          NumObservations = nrow(alignment_df),
                          SpeakerSubPop = alignment_df$subpop_num,
                          MarkerType = alignment_df$category_num,
@@ -52,17 +52,72 @@ alignment_data <- list(NumMarkers = length(unique(alignment_df$category)),
                          CountsNotAB = alignment_df$bna,
                          StdDev=sd )
 
-
-fit_model <- sampling(alignment_model, data = alignment_data, 
-                      iter = 500, warmup = 100, chains = 3 ,
-                      control = list(adapt_delta = .95))
-
-return(fit_model) 
+return(alignment_data) 
 }
+
+alignment_data <- fit_alignment(childes_data) 
 
 
 alignment_model = stan_model(file = "~/Documents/stanmodel.stan")
 
-alignment_data <- fit_alignment(childes_data) 
+fit<- sampling(alignment_model, data = alignment_data, 
+                iter = 500, warmup = 100, chains = 3 ,
+                control = list(adapt_delta = .95))
+
+#fit <- stan(file = alignment_model, data = alignment_data, 
+ #           iter = 500, chains =1 )
+
+mu_notab <- colMeans(rstan:::extract(fit,"mu_notab")$mu_notab)
+mu_ab <- colMeans(rstan:::extract(fit,"mu_ab")$mu_ab)
+
+d2<- childes_data
+#If the model works, its mu_ values should be "smoothed" estimates of p(B|A) & p(B|notA)
+par(mar = rep(2, 4))
+plot(mu_ab,d2$ba/(d2$ba+d2$nba))
+plot(mu_notab,d2$bna/(d2$bna+d2$nbna))
+
+#checking in on the learned subpopulation values
+eta_ab_subpop <- colMeans(rstan:::extract(fit,"eta_ab_subpop")$eta_ab_subpop)
+eta_subpop <- colMeans(rstan:::extract(fit,"eta_subpop")$eta_subpop) ######this doesn't exist
+
+d3 <- d2 %>%
+  mutate(model_eta = colMeans(rstan:::extract(fit,"eta_ab_observation")$eta_ab_observation))
+
+d3$model_mu <- log(colMeans(rstan:::extract(fit,"mu_ab")$mu_ab)) - 
+  log(colMeans(rstan:::extract(fit,"mu_notab")$mu_notab))
+
+d3$model_dnm <- colMeans(rstan:::extract(fit,"mu_ab")$mu_ab) - 
+  colMeans(rstan:::extract(fit,"mu_notab")$mu_notab)
+
+
+d4 <- d3 %>%
+  group_by(subpop) %>%
+  multi_boot_standard("model_eta", na.rm = F)
+
+ggplot(aes(x = mean, y = subpop), data = d4) +
+  geom_pointrange(aes(xmax = ci_upper, xmin = ci_lower)) +
+  geom_smooth(method = "loess") +
+  scale_color_brewer(palette = "Set1") +
+  theme_bw(base_size = 14) +
+  theme(panel.grid = element_blank())
+
+d4 <- d3 %>%
+  group_by(subpop, category) 
+
+#This is the plot for results/children_eta_bywords.pdf
+ggplot(aes(y = subpop, x = model_eta, colour = category,
+), data=d4) + 
+  geom_point()+
+  geom_smooth(method = "loess") +
+  scale_color_brewer(palette = "Set1") +
+  theme_bw(base_size = 14) +
+  theme(panel.grid = element_blank()) +
+  labs(title='Model-estimated Alignment to speaker pairs',
+       y='Speaker pairs',
+       x='Alignment (delta log-odds)',
+       colour='Alignment')
+
+
+
 
 
